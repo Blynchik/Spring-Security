8da19c7be426548c5f45dacf2a7de09d2e9899de
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-04-25 12:52:57 +0300

    7.4

--- src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
@@ -2,11 +2,13 @@ package com.zaurtregulov.spring.security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -33,5 +35,17 @@ public class MySecurityConfig {
                 .roles("MANAGER", "HR")
                 .build());
         return manager;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .requestMatchers("/hr_info").hasRole("HR")
+                .requestMatchers("/manager_info/**").hasRole("MANAGER")
+                .anyRequest().authenticated()
+                .and().formLogin().permitAll();
+
+        return http.build();
     }
 }
--- src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
+++ src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <%--
   Created by IntelliJ IDEA.
   User: Blynchik
@@ -11,16 +12,19 @@
     <title>Title</title>
 </head>
 <body>
-
 <h3>Information for all employees</h3>
 <br>
+<security:authorize access="hasRole('HR')">
 <input type="button" value="Salary"
        onclick="window.location.href = 'hr_info'">
 Only for HR stuff
+</security:authorize>
 <br>
+<security:authorize access="hasRole('MANAGER')">
 <input type="button" value="Performance"
        onclick="window.location.href = 'manager_info'">
 Only for managers
+</security:authorize>
 
 </body>
 </html>
